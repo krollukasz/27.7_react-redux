63f9d35b69469de0e15c540e3015a04a719bf2db
Łukasz Król <lionel@o2.pl>
2019-05-14 17:08:16 +0200

    Create production mode

---
+++ build/precache-manifest.09af2fa244649add95ba931e8ea1ba37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aea2e10de1cc1778b8d1605b3b530a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87da5b9d13d442869c46",
+    "url": "/static/css/main.1e989747.chunk.css"
+  },
+  {
+    "revision": "546867b79796684fa1c2",
+    "url": "/static/js/2.fd4d4cd7.chunk.js"
+  },
+  {
+    "revision": "87da5b9d13d442869c46",
+    "url": "/static/js/main.f854bd1d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
